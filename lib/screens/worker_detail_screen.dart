6f33b97d34ad5487846a3d5849a7ee8b4da0c46d
Tomas Baran (MacBook Air) <tomas.baran@gmail.com>
2022-10-30 14:57:25 -0600

    Add phone calling funcionality

--- lib/screens/worker_detail_screen.dart
+++ lib/screens/worker_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gapi/widgets/bottom_black_button.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 class WorkersDetailScreen extends StatelessWidget {
   final String categoryName;
@@ -12,25 +13,45 @@ class WorkersDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(categoryName)),
       body: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Container(
-          margin: EdgeInsets.all(32),
-          height: 120,
-          width: 120,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.all(
-              Radius.circular(32),
+        Expanded(
+          child: ListView(children: [
+            Container(
+              margin: EdgeInsets.all(32),
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(32),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  '? %',
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 32),
+                ),
+              ),
             ),
-          ),
-          child: Center(
-            child: Text(
-              '? %',
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 32),
+            Text(workerName),
+          ]),
+        ),
+        Center(
+          child: ElevatedButton(
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor)),
+            onPressed: () => UrlLauncher.launchUrl(Uri(scheme: 'tel', path: phoneNumber)),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.phone),
+                const SizedBox(
+                  width: 12,
+                ),
+                Text(phoneNumber),
+              ],
             ),
           ),
         ),
-        Text(workerName),
-        Text(phoneNumber),
+        const SizedBox(height: 12),
         BottomBlackButton(title: '+ AÑADIR EVALUACIÓN', onTap: () => null),
       ]),
     );
