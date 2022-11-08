4fe1d6ba8da9f911e3ae39d4badc01a570262bfa
Tomas Baran (MacBook Air) <tomas.baran@gmail.com>
2022-11-07 20:09:12 -0600

    UI changes

--- lib/widgets/comment.dart
+++ lib/widgets/comment.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:gapi/theme/style_constants.dart';
+import 'package:gapi/widgets/review_container.dart';
 import 'package:intl/intl.dart';
 
 class Comment extends StatelessWidget {
@@ -16,7 +18,7 @@ class Comment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         ClipRRect(
           borderRadius: BorderRadius.all(Radius.circular(12)),
@@ -24,16 +26,34 @@ class Comment extends StatelessWidget {
             padding: EdgeInsets.all(12),
             // color: kGreen,
             color: rating < 2.5 ? kColorRed : kColorGreen,
-            child: Text(
-              commentBody,
-              style: tsReviewCategoryComment,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  commentBody,
+                  style: tsReviewCategoryComment,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                RatingBarIndicator(
+                  unratedColor: Colors.transparent,
+                  rating: rating,
+                  itemSize: 8,
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.white70,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
         Text(
-          '${DateFormat('yyyy-MM-dd').format(date)}: $rating  stars',
+          '${DateFormat('yyyy-MM-dd').format(date)}',
           style: tsReviewCategoryCommentDate,
         ),
+        const SizedBox(height: 16),
       ],
     );
   }
